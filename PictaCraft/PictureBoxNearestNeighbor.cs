55c5b1a454390f5e824f543530d48b6bd24d423c
Rektroth <53879295+Rektroth@users.noreply.github.com>
2021-01-02 16:25:57 -0500

    Add files via upload

---
+++ PictaCraft/PictureBoxNearestNeighbor.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace PictaCraft
+{
+    /// <summary>
+    /// A picture box that resizes images with the nearest neighbor algorithm.
+    /// </summary>
+    public class PictureBoxNearestNeighbor : PictureBox
+    {
+        /// <summary>
+        /// Raises the Control.Paint event.
+        /// </summary>
+        /// <param name="e">The paint event arguments.</param>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+            e.Graphics.PixelOffsetMode = PixelOffsetMode.Half;
+            base.OnPaint(e);
+        }
+    }
+}
